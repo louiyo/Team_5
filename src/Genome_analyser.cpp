ce7bb86dfb3b71d384ddfd3f8230c1b4a2b8599d
Des Courtils Philippine Marie Joseph <descourt@INTRANET.EPFL.CH@CO-IN-SC-163.intranet.epfl.ch>
2018-11-14 17:47:15 +0100

    read fucntion is ready

--- src/Genome_analyser.cpp
+++ src/Genome_analyser.cpp
@@ -53,39 +53,38 @@ void write_seq_rev(std::ofstream& output, bool forward=true){
 
 void reader(std::string file_in)
 {
-    //on ouvre le fichier file_in qui est un attribut de la Grosse classe
     std::ifstream file_input(file_in.c_str());
-
-    //se crée tout seul
     std::ofstream file_output("name_output_file");
+    
+    std::string chromo_seq, line, last;
 
-
-      std::string chromo_name, chromo_seq, line;
-
-        if(file_input.is_open())
+        if(!file.eof())
         {
-            while(std::getline(file_input, line)) //modifier 
+            while(std::getline(file_input, line)) 
             {
-
-                // si la ligne est un nom de chromosome, on stocke le nom dans l'attribut chromosome_name
-                //on traite la séquence chromosique complète qui vient d'être extraite
                 if(line[0] == '>')
                 {
+                    chromosome_name = line;
+                    chromosome_name.erase(0,1);
+                    current_pos_in_chr = 0;
+                    last = "";
+					
+                }
 
-                    //si on a déjà une chromo_seq et qu'on est retourné à '>', alors c'est la fin du chromosome :
-                    //on peut commencer a découper la ligne extraite
-                    if(chromo_seq.size() != 0)
-                    {
-						if(chromo_seq.size() >= seq_size)
-						{
-                        //tant qu'on n'est pas à la fin du chromosome ATTENTION CAR IL SE PEUT QUIL Y AIT UNE COMPARAISON AVEC UN NB NEGATIF
+                else
+                {
+                    current_pos_in_chr = 0;
+                    chromo_seq = last;
+                    chromo_seq += line;
+                    
+                    if(chromo_seq.size() >= seq_size)
+					{
+                        last = line.substr(line.size()-seq_size + 1, seq_size);
+                        
                         while(current_pos_in_chr <= (chromo_seq.size()- seq_size) )
 						{
-                            //on extrait une séquence d'une longueur donnée (attribut se(la fonction extract_seq la stocke dans l'objet Sequence,
-                            //initialise la position courant ++1)
                             extract_seq(chromo_seq, seq_size);
 
-                        //on regarde si la séquence et ses paramètres peuvent être écrits dans le fichier de sortie
 							if(current_seq.score() > threshold)
 							{
 								writer(file_output);
@@ -98,58 +97,11 @@ void reader(std::string file_in)
 								writer(file_output, false);
 							}
 						}
-						
-						}
-
-                    }
-
-                    //on passe au chromosome suivant et on change le chromosome courant (en ayant au préalable supprimé le signe '>')
-                    chromo_name = line;
-                    chromo_name.erase(0,1);
-                    chromosome_name = chromo_name;
-                    current_pos_in_chr = 0;
-
-                    //on remet à 0 la séquence courante de chromosome
-                    chromo_seq = "";
-                }
-
-                    //si c'est une encore séquence, on l'ajoute (dans le cas ou la séq chromosomique est écrite sur plusieurs lignes)
-                else
-                {
-                    chromo_seq += line;
+					}   
                 }
 
 
             }
-            //pour prendre la dernière ligne du fichier
-			
-			if(chromo_seq.size() >= seq_size)
-			{
-            //tant qu'on n'est pas à la fin du chromosome ATTENTION CAR IL SE PEUT QUIL Y AIT UNE COMPARAISON AVEC UN NB NEGATIF
-            while(current_pos_in_chr <= (chromo_seq.size()- seq_size) )
-            
-            {
-
-                            //on extrait une séquence d'une longueur donnée (attribut se(la fonction extract_seq la stocke dans l'objet Sequence,
-                            //initialise la position courant ++1)
-                            extract_seq(chromo_seq, seq_size);
-
-           
-					if(current_seq.score() > threshold)
-                        {
-                            writer(file_output);
-                        }
-                        
-                        set_sequence(revert_seq());
-
-                        if(current_seq.score() > threshold)
-                        {
-                            writer(file_output, false);
-                        }
-             }
-			 }
-
-
         }
         //on ferme les fichiers
         file_input.close();
